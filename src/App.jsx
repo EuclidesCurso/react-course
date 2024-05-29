6c09aa22819de56a15b043d40e9a9cb3b8059415
EuclidesCurso <euclides.curso@gmail.com>
2024-05-28 21:50:21 -0300

    chore: added styled-components lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
-import TeamComposition from "./components/Team";
+import Square from "./styles/styled-components/components/Square";
+
 
 function App() {
     return (
     <body>
-      <TeamComposition/>
+      <Square />
+     
     </body>
   );
  }
