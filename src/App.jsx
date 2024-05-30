cca70ece44f0e7eedaa909bf484db6c8f3851145
EuclidesCurso <euclides.curso@gmail.com>
2024-05-29 22:08:50 -0300

    feat: created Logo component structure using composition pattern and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css"
+import Logo from "./styles/styled-components/components/Logo"
 import Square from "./styles/styled-components/components/Square";
 
 
 function App() {
     return (
     <body>
-      <Square />
+      <Logo />
      
     </body>
   );
