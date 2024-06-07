407c151ebd69702e888d9f72d3b5a5e947a89c1e
EuclidesCurso <euclides.curso@gmail.com>
2024-06-06 21:17:48 -0300

    feat: added dynamic props in DropdownMenu component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,36 @@ import Headline from "./styles/css-modules/components/Headline";
 import Logo from "./styles/styled-components/components/Logo"
 import Square from "./styles/styled-components/components/Square";
 import Header from "./styles/css-modules/components/Header";
+import DropdownMenu from "./Hooks/components/DropdownMenu";
 
 
 function App() {
+
+        const pages = [
+          {
+          title: "Page one",
+          description: "Lorem ipsum, dolor sit amet consectetur adipisicing."
+          },
+          {
+          title: "Page one",
+          description: "Lorem ipsum, dolor sit amet consectetur adipisicing."
+          },
+          {
+          title: "Page one",
+          description: "Lorem ipsum, dolor sit amet consectetur adipisicing."
+          },
+          {
+          title: "Page one",
+          description: "Lorem ipsum, dolor sit amet consectetur adipisicing."
+          },
+
+
+
+        ]
     return (
     <body>
-      <Header/>
+     
+      <DropdownMenu pages={pages} />
     </body>
   );
  }
